83b291439fc8870fb6662164dcfecc7b7801254d
BrandonA614 <brandon.ashkin@darbygroup.com>
2016-09-06 09:08:12 -0600

    Completed with added Question comfortability

--- MathQuizzer/src/MathQuizzer.java
+++ MathQuizzer/src/MathQuizzer.java
@@ -37,18 +37,22 @@ public class MathQuizzer
 			public static void MathQuizOne(){
 				
 				//Number Gens
-				int RandomNumber = new Random().nextInt(6) + 1;
-				int randomNumberTwo = new Random().nextInt(6) + 1;
+				int RandomNumber = new Random().nextInt(10) + 1;
+				int randomNumberTwo = new Random().nextInt(10) + 2;
 				
 				//Asks for Input and Outputs initial Questions
-				System.out.println("Given ** Random().nextInt(* " + RandomNumber + ") + " + randomNumberTwo + "");
+				System.out.println("Given ** Random().nextInt(" + randomNumberTwo + ") + " + RandomNumber + "");
+				System.out.println("What would the Lower Bound and Uppper Bound number be?");
 				Scanner answerInput = new Scanner(System.in);
 				int answerAmount = answerInput.nextInt(); 
 				Scanner answerInputTwo = new Scanner(System.in);
 				int answerAmountTwo = answerInputTwo.nextInt(); 
+//				int answerAmountTwoFinal = answerAmount + answerAmountTwo;
+//				int randomNumberTwoFinal = RandomNumber + randomNumberTwo;
+				int randomNumberFinal = RandomNumber + randomNumberTwo - 1 ;
 				
 				//Outputs Whether the player was correct or incorrect
-				if(RandomNumber + randomNumberTwo == answerAmount + answerAmountTwo){
+				if(RandomNumber == answerAmount && randomNumberFinal  == answerAmountTwo){
 					System.out.println("You got it right!");
 					counter++;
 					System.out.println("Your score is currently " + counter);
@@ -63,13 +67,14 @@ public class MathQuizzer
 			public static void MathQuizTwo(){
 				
 				
-				int RandomNumberQuizTwo = new Random().nextInt(10) + 1;
-				int randomNumberQuizTwoTwo = new Random().nextInt(RandomNumberQuizTwo) + 1;
+				int lowerBound = new Random().nextInt(10) + 1;
+				int upperBound = new Random().nextInt(10) + 10;
+				
 				//Initial Question
 				System.out.println("");
-				System.out.println("Given int random number = (int)(Math.random() * X) + Y)");
+				System.out.println("Given int random number = new Random().nextInt(X) + Y");
 				System.out.println("");
-				System.out.println("What would X and Y be if I was looking for a range between " + RandomNumberQuizTwo +" and "  + randomNumberQuizTwoTwo);
+				System.out.println("What would X and Y be if I was looking for a range between " + lowerBound +" and "  + upperBound);
 				System.out.println("");
 				
 				//Scans for User Input for their answers
@@ -77,13 +82,17 @@ public class MathQuizzer
 				int answer = answerInput.nextInt();
 				Scanner answerInputTwo = new Scanner(System.in);
 				int answerTwo = answerInputTwo.nextInt();
+				int upperBoundFull = upperBound - lowerBound + 1;
+				 
+				
 				//Basically outputs right or wrong and score
-				if(answer + answerTwo == RandomNumberQuizTwo + randomNumberQuizTwoTwo){
+				if(answer == lowerBound && answerTwo == upperBoundFull){
 					System.out.println("You got it right!");
 					counter++;
 					System.out.println("Your score is currently " + counter);
 				}else {
 					System.out.println("You got it wrong!");
+					System.out.println("The correct answer was X = " + upperBoundFull + " and Y = " + lowerBound );
 					counter--;
 					System.out.println("Your score is currently " + counter);
 				}
